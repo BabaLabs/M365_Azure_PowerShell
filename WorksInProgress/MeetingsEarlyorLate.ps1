6c4c127d422003caf25ce95daea23486eefe0593
Im Baba <108435751+BabaLabs@users.noreply.github.com>
2024-09-22 12:29:48 -0400

    Delete WorksInProgress/MeetingsEarlyorLate.ps1

--- WorksInProgress/MeetingsEarlyorLate.ps1
+++
@@ -1,7 +0,0 @@
-#IDK do something with this and make it a script for Boujie customers
-
-Set-OrganizationConfig -DefaultMinutestoReduceLongEventsBy 5
-
-Set-OrganizationConfig -DefaultMinutestoReduceShortEventsBy 5
-
-Set-OrganizationConfig -ShortenEventScopeDefault EndEarly
